d41bdcfb3af4ba098a689378ecde7c3d8dae1317
andrei <balasandrei200@yahoo.com>
2023-07-27 20:15:11 +0300

    commit

--- routes/register.js
+++ routes/register.js
@@ -10,6 +10,8 @@ let object1 = {};
 object.link = process.env.CYCLIC_URL;
 object.name = 'Home';
 currentUrl.push(object);
+let womansCategoriesButton = process.env.CYCLIC_URL + '/womansClothing';
+let mensCategoriesButton = process.env.CYCLIC_URL + '/mensClothing';
 
 object1.link = `${process.env.CYCLIC_URL}/register`;
 object1.name = 'Registration';
@@ -20,6 +22,8 @@ router.get('/', function (req, res, next) {
     title: 'Robbing City Galati',
     homePageUrl: process.env.CYCLIC_URL,
     currentUrl: currentUrl,
+    womanButton: womansCategoriesButton,
+    mensButton: mensCategoriesButton,
   });
 });
 module.exports = router;
